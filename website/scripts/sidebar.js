d74de83e5cf7f1e414141e03f1b31d20df08c977
Ninsang Sherpa <18sherpan@gmail.com>
2026-04-23 12:31:00 +0100

    sidebar fix

--- website/scripts/sidebar.js
+++ website/scripts/sidebar.js
@@ -3,12 +3,7 @@ let slide = document.getElementById("newtab")
 let value = -99.9
 
 button.addEventListener("click", ()=> {
-    value = (value === -99.9) ? -40 : -99.9;
-    /*if (value == -99.9) {
-        value = -10
-    } else {
-        value = -99.9
-    }*/
+    value = (value === -99.9) ? -20 : -99.9;
     slide.style.right = String(value) + "%";
     console.log(slide.style.right)
     console.log(value)
